40677541c6b7c2e130aee3a6d32108703f08f772
pragatwagle <52867070+pragatwagle@users.noreply.github.com>
2021-11-14 07:28:24 -0500

    Files Added for Connector without board
    game without board added

---
+++ connector-games/game-without-board/game/client_types/phantom.js
@@ -0,0 +1,19 @@
+/**
+ * # Phantom type implementation of the game stages
+ * Copyright(c) 2020 Jasper Wilson <jaspermwilson@gmail.com>
+ * MIT Licensed
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+var ngc = require('nodegame-client');
+var stepRules = ngc.stepRules;
+var constants = ngc.constants;
+
+module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
+
+    // Must implement the stages here.
+
+    // ...
+
+};
