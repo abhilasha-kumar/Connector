6059ccff9d2024549aa1ab0846e623c2937f901d
abhilasha-kumar <abhilashak9@gmail.com>
2022-02-07 14:17:37 -0500

    fixed functions

--- search-models/search_funcs.py
+++ search-models/search_funcs.py
@@ -301,7 +301,7 @@ class RSA:
             for board_name in boards.keys()}
       for (key, embedding) in representations.items()
     }
-    representation = representations['glove']
+    representation = list(representations.keys())
     boardmatrix = board_matrices[representation][board_name]
     ## here we restrict the softmax to only certain candidates
     candidate_index = [list(vocab["vocab_word"]).index(w) for w in candidates]
