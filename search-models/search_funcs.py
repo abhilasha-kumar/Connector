0ae969166f45e9d338c37c61362c9634ef8f15bd
abhilasha-kumar <abhilashak9@gmail.com>
2022-02-07 13:09:17 -0500

    fixed indents

--- search-models/search_funcs.py
+++ search-models/search_funcs.py
@@ -27,18 +27,18 @@ from scipy.special import softmax
 
 class search:
   def create_similarity_matrix(matrix):
-  '''
-  inputs:
-  (1) matrix: a numpy array with vectors for which the NxN similarity matrix needs to be created
-  output:
-  a NxN similarity matrix
-  
-  '''
-  import scipy.spatial.distance
-  N = matrix.shape[0]
-  matrix = 1-scipy.spatial.distance.cdist(matrix, matrix, 'cosine').reshape(-1)
-  matrix = matrix.reshape((N,N))
-  return matrix
+    '''
+    inputs:
+    (1) matrix: a numpy array with vectors for which the NxN similarity matrix needs to be created
+    output:
+    a NxN similarity matrix
+    
+    '''
+    import scipy.spatial.distance
+    N = matrix.shape[0]
+    matrix = 1-scipy.spatial.distance.cdist(matrix, matrix, 'cosine').reshape(-1)
+    matrix = matrix.reshape((N,N))
+    return matrix
 
   def random_walk(word, n_steps, vocab, Graph):
     '''
